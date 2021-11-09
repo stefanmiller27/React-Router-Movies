2b125f589e2cab24c1b056f6ad249eb5a33e681b
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2021-11-09 01:43:08 -0500

    this was a little confusing

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-document';
 
 export default function SavedList(props) {
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
-    </div>
-  );
-}
+       <Link to = '/' className = 'home-button'>Home</Link>
+        </div>
+    );
+} 
